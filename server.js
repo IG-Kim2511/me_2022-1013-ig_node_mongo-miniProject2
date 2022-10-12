1c56810139341721a8332cb0cb97a1e676b6d66c
IG <iikim2511@gmail.com>
2022-10-12 14:07:30 -0400

    clear

--- server.js
+++ server.js
@@ -5,18 +5,13 @@ const app = express();
 // colors
 let colors = require("colors");
 
-// c24-5) bodyParser
-let bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-
 // dotenv
 require('dotenv').config()
 
 // c30) mogoClient
 let MongoClient = require('mongodb').MongoClient;
 
-
- // c32) ejs
+// c32) ejs
 // let ejs = require('ejs'); 
 app.set('view engine','ejs')
 
@@ -25,28 +20,8 @@ var expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
 
-
 // c50) static
 app.use(express.static('public'))
-
-// c52)  method-override
-let methodOverride = require('method-override')
-app.use(methodOverride('_method'))
-  
-// 🍀c58-10)
-// passport
-const passport = require('passport');
-
-// passport-local
-const LocalStrategy = require('passport-local').Strategy;
-
-// express-session
-const session = require('express-session');
-
-// middleware
-app.use(session({ secret: 'ig123', resave: true, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 
