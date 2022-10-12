39ce9b3cf38ea291387fb96a4ac7ae05f54a0889
IG <iikim2511@gmail.com>
2022-10-12 17:12:28 -0400

    stop for now

--- server.js
+++ server.js
@@ -23,35 +23,54 @@ app.set('layout', 'layouts/main_layout.ejs');
 // c50) static
 app.use(express.static('public'))
  
-// mongoose
-require('./server/mongoose_controller.js');
+var url = process.env.mongoDB_url;
 
-// 🍀🍀🍀app.get
-app.get("/", function (req, res) {
-    res.render('index.ejs')  
+MongoClient.connect(url, function(err, client) {
+  if (err) throw err;
+  console.log(("Database created!").bgMagenta);
+
+  let db = client.db('db0929')
+
+    // 🍀🍀🍀app.get
+    app.get("/", function (req, res) {
+        res.render('index.ejs')  
+    });
+
+    app.get("/list", function (req, res) {
+        // find().toArray()
+        db.collection('post').find().toArray(function (err,pp_res) {
+            console.log((`list`).bgBlue)
+            console.log(pp_res)
+            
+            // ejs
+            //res.render
+            res.render('list.ejs',{ig_posts:pp_res,  meta_title:'miniProject'});
+        })
+    });
+
+    app.get("/write", function (req, res) {
+        res.render('write.ejs')
+    });
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.listen(process.env.PORT || 8080, function () {
+        console.log((`bgMagenta`).bgMagenta)
+        console.log(`ig node server gogo, port: ${process.env.PORT}`.rainbow);    
+    });
+    //   db.close();
 });
 
-app.get("/list", function (req, res) {
-    res.render('list.ejs',{ig_posts:res});
 
-    // // find().toArray()
-    // db.collection('post').find().toArray(function (err,pp_res) {
-    //   console.log((`list`).bgBlue)
-    //   console.log(pp_res)
-      
-    //   // ejs
-    //   //res.render
-    //   res.render('list.ejs',{ig_posts:pp_res,  meta_title:'miniProject'});
-    // })
-});
-
-app.get("/write", function (req, res) {
-    res.render('write.ejs')
-});
-
-
-
-app.listen(process.env.PORT || 8080, function () {
-    console.log((`bgMagenta`).bgMagenta)
-    console.log(`ig node server gogo, port: ${process.env.PORT}`.rainbow);    
-});
