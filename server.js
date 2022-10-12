a1105c05b8ce3fa1b8a55d1713f8ae27beeb9f75
IG <iikim2511@gmail.com>
2022-10-12 15:08:42 -0400

    bootstrap-write  -ing

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set('view engine','ejs')
 // express-ejs-layouts
 var expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
-app.set('layout', 'layouts/layout');
+app.set('layout', 'layouts/main_layout.ejs');
 
 // c50) static
 app.use(express.static('public'))
@@ -27,8 +27,27 @@ app.use(express.static('public'))
 
 
 app.get("/", function (req, res) {
-    res.render('index.ejs',{meta_title:'miniProject'})  
+    res.render('index.ejs')  
 });
+
+app.get("/list", function (req, res) {
+    res.render('list.ejs',{ig_posts:res});
+
+    // // find().toArray()
+    // db.collection('post').find().toArray(function (err,pp_res) {
+    //   console.log((`list`).bgBlue)
+    //   console.log(pp_res)
+      
+    //   // ejs
+    //   //res.render
+    //   res.render('list.ejs',{ig_posts:pp_res,  meta_title:'miniProject'});
+    // })
+});
+
+app.get("/write", function (req, res) {
+    res.render('write.ejs')
+});
+
 
 
 app.listen(process.env.PORT || 8080, function () {
